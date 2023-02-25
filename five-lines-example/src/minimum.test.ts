544a0063b887f775d654f1a1c90d8d458a7a3584
jihwooon <jihwooon@gmail.com>
2023-02-26 01:52:39 +0900

    minimum 함수를 리팩토링하라

--- five-lines-example/src/minimum.test.ts
+++ five-lines-example/src/minimum.test.ts
@@ -2,13 +2,18 @@ export const minimum = (arr: number[][]) => {
     let result = Number.POSITIVE_INFINITY;
     for(let x = 0; x < arr.length; x++) {
         for(let y = 0; y < arr[x].length; y++) {
-            if(result > arr[x][y]) {
-                result = arr[x][y]
-            }
+            result = min(result, arr, x, y);
         }
 
         return result
     }
+}
+
+function min(result: number, arr: number[][], x: number, y: number) {
+    if (result > arr[x][y]) {
+        result = arr[x][y];
+    }
+    return result;
 }
 
 test('minimum', () => {
