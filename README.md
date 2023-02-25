ad2854c424df9af8358ed9af0204910bbdae4315
jihwooon <jihwooon@gmail.com>
2023-02-25 19:26:25 +0900

    first-init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Five-Lines-Of-Codes
+
---
+++ five-lines-example/composition.ts
@@ -0,0 +1,10 @@
+function containsEven(arr: number[][]) {
+    for(let x = 0; x < arr.length; x++) {
+        for(let y = 0; y < arr[x].length; y++) {
+            if (arr[x][y] % 2 === 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
