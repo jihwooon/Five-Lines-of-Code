37a8e36dedd21afd749d2da504f8fbe533d13666
jihwooon <jihwooon@gmail.com>
2023-02-26 02:29:44 +0900

    reportPrimes 함수를 구현하라

---
+++ five-lines-example/src/reportPrimes.test.ts
@@ -0,0 +1,19 @@
+const reportPrimes = (n: number) => {
+    for(let i = 2; i < n; i++) {
+        reportIfPrime(i);
+    }
+}
+
+const isPrime = (n: number) => {
+    return true;
+}
+
+function reportIfPrime(i: number) {
+    if (isPrime(i)) {
+        console.log(`${i}i is prime`);
+    }
+}
+
+test('reportPrimes', () => {
+    expect(reportPrimes(5)).toBe(2);
+})
