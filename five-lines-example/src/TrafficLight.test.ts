d8b9846799f598d6204c32861f4a82ba8a8efed6
jihwooon <jihwooon@gmail.com>
2023-03-09 17:37:08 +0900

    인터페이스에 새로운 기능을 추가하라

--- five-lines-example/src/TrafficLight.test.ts
+++ five-lines-example/src/TrafficLight.test.ts
@@ -2,28 +2,33 @@ enum RawTrafficLight {
     RED, YELLOW, GREEN
 }
 
+//TODO 인터페이스에 새로운 기능을 추가하라
 interface TrafficLight {
-    isRed(): boolean;
-    isYellow(): boolean;
-    isGreen(): boolean; 
+    isRed(): boolean
+    isYellow(): boolean
+    isGreen(): boolean
+    updateCar(): void
 }
 
 class Red implements TrafficLight {
     isRed(): boolean { return true }
     isYellow(): boolean { return false }
     isGreen(): boolean { return false }
+    updateCar() {}
 }
 
 class Yellow implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return true }
     isGreen(): boolean { return false }
+    updateCar() {}
 }
 
 class Green implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return false }
     isGreen(): boolean { return true }
+    updateCar() {}
 }
 
 const CYCLE = [new Red(), new Yellow(), new Green()]
