17da639d6ee14ef3ba49f10177bb2b8977b2beb0
jihwooon <jihwooon@gmail.com>
2023-03-09 16:12:32 +0900

    열거형의 나머지를 정리하라

--- five-lines-example/src/TrafficLight.test.ts
+++ five-lines-example/src/TrafficLight.test.ts
@@ -2,33 +2,32 @@ enum RawTrafficLight {
     RED, YELLOW, GREEN
 }
 
-interface TrafficLight2 {
+interface TrafficLight {
     isRed(): boolean;
     isYellow(): boolean;
     isGreen(): boolean; 
 }
 
-class Red implements TrafficLight2 {
+class Red implements TrafficLight {
     isRed(): boolean { return true }
     isYellow(): boolean { return false }
     isGreen(): boolean { return false }
 }
 
-class Yellow implements TrafficLight2 {
+class Yellow implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return true }
     isGreen(): boolean { return false }
 }
 
-class Green implements TrafficLight2 {
+class Green implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return false }
     isGreen(): boolean { return true }
 }
 
-const CYCLE = [RawTrafficLight.RED, RawTrafficLight.YELLOW, RawTrafficLight.GREEN]
-//TODO 이전 타입에서 임시 이름으로 변경하고 일치 여부 검사를 하라
-function updateCarForLight(current: TrafficLight2) {
+const CYCLE = [new Red(), new Yellow(), new Green()]
+function updateCarForLight(current: TrafficLight) {
     if(current.isRed()) {
         // car.stop()
     } else {
