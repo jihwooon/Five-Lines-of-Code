15558e7575aa95f98f48151221ba25b56d9d6e1b
jihwooon <jihwooon@gmail.com>
2023-03-09 15:57:04 +0900

    신호등 신호를 가리키는 열거형과 차가 지나가도 되는지 여부 결정하를 함수를 생성하라

---
+++ five-lines-example/src/TrafficLight.test.ts
@@ -0,0 +1,12 @@
+enum TrafficLight {
+    RED, YELLOW, GREEN
+}
+
+const CYCLE = [TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN]
+function updateCarForLight(current: TrafficLight) {
+    if(current === TrafficLight.RED) {
+        // car.stop()
+    } else {
+        // car.drive()
+    }
+}
