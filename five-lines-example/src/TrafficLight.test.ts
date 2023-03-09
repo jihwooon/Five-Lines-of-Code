5940be2d2e274c9538f4907ad878e724e40bba02
jihwooon <jihwooon@gmail.com>
2023-03-09 16:02:12 +0900

    각 열거형 값에 대한 클래스를 생성하라
    열거형에 대한 각 클래스를 생성하고 해당 메서드를 제외한 인터페이스의
    모든 메서드를 false로 반환합니다.

--- five-lines-example/src/TrafficLight.test.ts
+++ five-lines-example/src/TrafficLight.test.ts
@@ -1,4 +1,3 @@
-//TODO enum 타입에서 interface으로 도입하라
 enum TrafficLight {
     RED, YELLOW, GREEN
 }
@@ -7,6 +6,25 @@ interface TrafficLight2 {
     isRed(): boolean;
     isYellow(): boolean;
     isGreen(): boolean; 
+}
+
+//TODO 각 열거형 값에 대한 클래스를 생성하라
+class Red implements TrafficLight2 {
+    isRed(): boolean { return true }
+    isYellow(): boolean { return false }
+    isGreen(): boolean { return false }
+}
+
+class Yellow implements TrafficLight2 {
+    isRed(): boolean { return false }
+    isYellow(): boolean { return true }
+    isGreen(): boolean { return false }
+}
+
+class Green implements TrafficLight2 {
+    isRed(): boolean { return false }
+    isYellow(): boolean { return false }
+    isGreen(): boolean { return true }
 }
 
 const CYCLE = [TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN]
