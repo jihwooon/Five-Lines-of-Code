445121ea7fd74b0dbc499a0815e1f33cca8d1511
jihwooon <jihwooon@gmail.com>
2023-03-09 15:59:15 +0900

    임시 이름으로 인터페이스를 도입하라
    인터페이스에 열거형의 각 값에 대한 메서드를 포함해야 합니다.

--- five-lines-example/src/TrafficLight.test.ts
+++ five-lines-example/src/TrafficLight.test.ts
@@ -1,5 +1,12 @@
+//TODO enum 타입에서 interface으로 도입하라
 enum TrafficLight {
     RED, YELLOW, GREEN
+}
+
+interface TrafficLight2 {
+    isRed(): boolean;
+    isYellow(): boolean;
+    isGreen(): boolean; 
 }
 
 const CYCLE = [TrafficLight.RED, TrafficLight.YELLOW, TrafficLight.GREEN]
