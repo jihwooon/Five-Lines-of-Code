b7065c341eeba508e0592ca1b6b75b3679f4c1a2
jihwooon <jihwooon@gmail.com>
2023-03-09 17:59:07 +0900

    클래스에 맞게 조건식들을 수정하라

--- five-lines-example/src/TrafficLight.test.ts
+++ five-lines-example/src/TrafficLight.test.ts
@@ -9,18 +9,12 @@ interface TrafficLight {
     updateCar(): void
 }
 
-//TODO 
 class Red implements TrafficLight {
     isRed(): boolean { return true }
     isYellow(): boolean { return false }
     isGreen(): boolean { return false }
-    updateCar(): void {}
-    updateCarForLight() {
-        if(true) {
-            // car.stop()
-        } else {
-            // car.drive()
-        }
+    updateCar() {
+        // car.stop()
     }
 }
 
@@ -28,13 +22,8 @@ class Yellow implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return true }
     isGreen(): boolean { return false }
-    updateCar() {}
-    updateCarForLight() {
-        if(false) {
-            // car.stop()
-        } else {
-            // car.drive()
-        }
+    updateCar() {
+        // car.drive()
     }
 }
 
@@ -42,21 +31,12 @@ class Green implements TrafficLight {
     isRed(): boolean { return false }
     isYellow(): boolean { return false }
     isGreen(): boolean { return true }
-    updateCar() {}
-    updateCarForLight() {
-        if(false) {
-            // car.stop()
-        } else {
-            // car.drive()
-        }
+    updateCar() {
+        // car.drive()
     }
 }
 
 const CYCLE = [new Red(), new Yellow(), new Green()]
 function updateCarForLight(current: TrafficLight) {
-    if(current.isRed()) {
-        // car.stop()
-    } else {
-        // car.drive()
-    }
+    current.updateCar()
 }
